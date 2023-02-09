bc73ed363c2d0510edf45754c24c2fd15f7be4ff
Danny <zx320320@example.com>
2023-02-10 01:21:27 +0800

    add formik and yup authentication

--- src/components/RightBar/RightBar.tsx
+++ src/components/RightBar/RightBar.tsx
@@ -1,10 +1,13 @@
+import Card from "../ui/card/Card";
 import SideBar from "../ui/sideBar/SideBar";
 import "./RightBar.scss";
 
 const RightBar = () => {
   return (
     <SideBar>
-      <div className="rightBar">Right Bar</div>
+      <Card>
+        <div className="rightBar">Right Bar</div>
+      </Card>
     </SideBar>
   );
 };
