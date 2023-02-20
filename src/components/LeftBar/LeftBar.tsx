21d91a89a75be50b9f0c54874afcc7b3bbc92139
Danny <zx320320@example.com>
2023-02-21 01:11:44 +0800

    add iconLink

--- src/components/LeftBar/LeftBar.tsx
+++ src/components/LeftBar/LeftBar.tsx
@@ -1,17 +1,62 @@
 import SideBar from "../ui/sideBar/SideBar";
 import "./LeftBar.scss";
 import PeopleAltIcon from "@mui/icons-material/PeopleAlt";
+import avatar from "../../assets/avatars/avatar-1.jpg";
+import AccessTimeIcon from "@mui/icons-material/AccessTime";
+import EventNoteIcon from "@mui/icons-material/EventNote";
+import OndemandVideoIcon from "@mui/icons-material/OndemandVideo";
+import GroupsIcon from "@mui/icons-material/Groups";
+import { Link } from "react-router-dom";
 const LeftBar = () => {
   return (
     <SideBar>
       <div className="leftBar">
-        <div className="iconLink">
-          <PeopleAltIcon />
-          <p>Friends</p>
+        <div className="linkContainer">
+          <Link to="/">
+            <div className="iconLink avatar">
+              <img src={avatar} />
+              <p>user</p>
+            </div>
+          </Link>
+          <Link to="/friends">
+            <div className="iconLink">
+              <PeopleAltIcon />
+              <p>Friends</p>
+            </div>
+          </Link>
+          <Link to="/groups">
+            <div className="iconLink">
+              <GroupsIcon />
+              <p>Groups</p>
+            </div>
+          </Link>
+          <Link to="/watch">
+            <div className="iconLink">
+              <OndemandVideoIcon />
+              <p>Watch</p>
+            </div>
+          </Link>
+          <Link to="/events">
+            <div className="iconLink">
+              <EventNoteIcon />
+              <p>Events</p>
+            </div>
+          </Link>
+          <Link to="/recent">
+            <div className="iconLink">
+              <AccessTimeIcon />
+              <p>Most Recent</p>
+            </div>
+          </Link>
         </div>
-        <div className="iconLink">
-          <PeopleAltIcon />
-          <p>Friends</p>
+        <div className="shortcuts">
+          <div>
+            <h4>Your shortcuts</h4>
+          </div>
+          <div className="iconLink">
+            <AccessTimeIcon />
+            <p>Most Recent</p>
+          </div>
         </div>
       </div>
     </SideBar>
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,6 +5,10 @@ import WbSunnyOutlinedIcon from "@mui/icons-material/WbSunnyOutlined";
 import { useAppDispatch, useAppSelector } from "../../app/hooks/hooks";
 import { themeToggle } from "../../redux/themeSlice";
 import { Link } from "react-router-dom";
+import SearchIcon from "@mui/icons-material/Search";
+import ChatBubbleOutlineIcon from "@mui/icons-material/ChatBubbleOutline";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import avatar from "../../assets/avatars/avatar-1.jpg";
 
 const Navbar = () => {
   const dispatch = useAppDispatch();
@@ -15,14 +19,21 @@ const Navbar = () => {
         <Link to={"/"}>
           <span>PAPAYA</span>
         </Link>
+        <div className="search">
+          <SearchIcon />
+          <input type="text" id="search" placeholder="Search" />
+        </div>
       </div>
       <div className="right">
-        <HomeOutlinedIcon />
         {theme === "" ? (
           <DarkModeOutlinedIcon onClick={() => dispatch(themeToggle())} />
         ) : (
           <WbSunnyOutlinedIcon onClick={() => dispatch(themeToggle())} />
         )}
+        <HomeOutlinedIcon />
+        <ChatBubbleOutlineIcon />
+        <NotificationsNoneIcon />
+        <img src={avatar} className="avatar" />
       </div>
     </div>
   );
